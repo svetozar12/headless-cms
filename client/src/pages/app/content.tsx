4792659505bdf547d6a1adba544d4eb4d3e07047
svetozar12 <zaauna@abv.bg>
2023-01-18 15:05:22 -0800

    added auth in the frontend, now will rewrite api in go

--- client/src/pages/app/content.tsx
+++ client/src/pages/app/content.tsx
@@ -1,13 +1,11 @@
 import { GetServerSideProps, NextPage } from "next";
 import { getSession } from "next-auth/react";
-import { useCookie } from "next-cookie";
 import { redirectTo } from "../../utils/redirect";
 
 const ContentPage: NextPage = () => <div>hi</div>;
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const session = getSession();
-  console.log(session, "zvesden");
+  const session = await getSession({ ctx });
 
   if (!session) {
     return redirectTo("/");
